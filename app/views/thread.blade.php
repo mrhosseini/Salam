fc5ffa9f2ba5fca3c7b94ebf93e85416ebd89108
Mohammad Reza <mrhosseini1367@gmail.com>
2014-08-11 21:32:48 +0430

    fixed problem of thread view in mobile displays

--- app/views/thread.blade.php
+++ app/views/thread.blade.php
@@ -22,23 +22,23 @@
 					@foreach ($posts as $post)
 						<div class="row" style="padding-bottom: 30px;">
 							<div class="row" style="background-color: #f3f3f3; padding-top: 2px; padding-bottom: 2px; margin-left:0px; margin-right:0px; border-top: 1px solid #ddd;">
-								<div class="col-md-1 col-xs-1">
+								<div class="col-md-1 col-xs-2">
 									<a  href="{{ URL::to('/user/'.$post->user_id) }}">
 										<img src="{{ URL::to(Constants::$profile_pics_path.$post->user->profile->img) }}" style="width: 45px;" class="img-thumbnail">
 									</a>
 								</div>
-								<div class="col-md-9 col-xs-9" style="padding-top: 10px; font-family: serif; font-weight: bold;">
+								<div class="col-md-9 col-xs-7" style="padding-top: 10px; font-family: serif; font-weight: bold;">
 									{{ $post->user->profile->firstname }}
 									{{ $post->user->profile->lastname }}
 								</div>
-								<div class="col-md-2" style="text-align: left; padding-top: 10px;">
+								<div class="col-md-2 col-xs-3" style="text-align: left; padding-top: 10px;">
 									<span class="glyphicon glyphicon-time"></span>
 									{{ Helpers::digits2Persian(jDate::forge($post->created_at)->shortAgo()) }}
 								</div>
 							</div>
 							<div class ="row">
-								<div class="col-md-1"></div>
-								<div class="col-md-10" style="padding-top: 15px; line-height: 2em;">{{ $post->body }}</div>
+								<div class="col-md-1 col-xs-1"></div>
+								<div class="col-md-10 col-xs-10" style="padding-top: 15px; line-height: 2em;">{{ $post->body }}</div>
 							</div>
 						</div>
 					@endforeach
