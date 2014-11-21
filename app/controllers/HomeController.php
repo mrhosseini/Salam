373b4fc2e08cc448fd1501e4f5a262f33d4793c7
Mohammad Reza <mrhosseini1367@gmail.com>
2014-11-21 15:11:06 +0330

    fixing empty thread viewing problem

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -33,6 +33,7 @@ class HomeController extends BaseController {
 		if ($threads->isEmpty()){
 			return Redirect::to("/");
 		}
+// 		dd($threads);
 			
 		/*
 		 * Fetch threads in next page to see if current page is the last page
@@ -56,52 +57,59 @@ class HomeController extends BaseController {
 		foreach ($threads as $thread){
 			$posts = Post::select('user_id')->where('thread_id', '=', $thread->id)->orderBy('created_at')->get();
 			$thread_post_count["$thread->id"] = $posts->count();
+			
+			if ($thread_post_count["$thread->id"] > 0) {
+				
 
- 			$count_authors = $posts->count();
- 			
- 			/*
- 			 * save first and last authors. first author must be always the first in list
- 			 */
- 			$first = $posts->first()->user_id;
- 			$author_list["$thread->id"][0] = User::find($first);
-			$last = $posts->last()->user_id;
-			
-			/*
-			 * get post count for each author and sort descending
-			 */
-			$post_authors = Post::select('user_id', DB::raw('count(*) as count'))
-						->where('thread_id', '=', $thread->id)
-						->orderBy('count', 'desc')
-						->orderBy('created_at')
-						->groupBy('user_id')
-						->get();
-			$count_unique = $post_authors->count();
-			
-			$max_index = Constants::$author_list_capacity - 1;
-			$current_index = 1;
-			
- 			if ($count_unique <= Constants::$author_list_capacity){
+				$count_authors = $posts->count();
+				
 				/*
-				 * if unique authors less than Constants::$author_list_capacity all must be in list
-				 */
-				$max_index = $count_unique - 1;
- 			}
-
-			if ($first != $last){ 
+				* save first and last authors. first author must be always the first in list
+				*/
+				$first = $posts->first()->user_id;
+				$author_list["$thread->id"][0] = User::find($first);
+				$last = $posts->last()->user_id;
+				
 				/*
-				 * if first and last authors are different then put the last author in its place
-				 */
-				$author_list["$thread->id"][$max_index] = User::find($last);
-				$max_index--;
-			}
-			
- 			foreach ($post_authors as $post){
-				if ($post->user_id != $first && $post->user_id != $last){
-					$author_list["$thread->id"][$current_index] = User::find($post->user_id);
-					$current_index++;
-					if ($current_index >  $max_index)
-						break;
+				* get post count for each author and sort descending
+				*/
+				$post_authors = Post::select('user_id', DB::raw('count(*) as count'))
+							->where('thread_id', '=', $thread->id)
+							->orderBy('count', 'desc')
+							->orderBy('created_at')
+							->groupBy('user_id')
+							->get();
+				$count_unique = $post_authors->count();
+				
+				$max_index = Constants::$author_list_capacity - 1;
+				$current_index = 1;
+				
+				if ($count_unique <= Constants::$author_list_capacity){
+					/*
+					* if unique authors less than Constants::$author_list_capacity all must be in list
+					*/
+					$max_index = $count_unique - 1;
 				}
+
+				if ($first != $last){ 
+					/*
+					* if first and last authors are different then put the last author in its place
+					*/
+					$author_list["$thread->id"][$max_index] = User::find($last);
+					$max_index--;
+				}
+				
+				foreach ($post_authors as $post){
+					if ($post->user_id != $first && $post->user_id != $last){
+						$author_list["$thread->id"][$current_index] = User::find($post->user_id);
+						$current_index++;
+						if ($current_index >  $max_index)
+							break;
+					}
+				}
+			}
+			else {
+				$author_list["$thread->id"] = array();
 			}
 		}
 		 
